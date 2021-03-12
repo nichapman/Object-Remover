e49caff71dcce73cfac9fc8111e003c25e7b5a3f
Nick Chapman <nc654@bath.ac.uk>
2021-03-12 15:12:17 +0000

    updating fetch link

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ function processImage(e) {
     bctx.clearRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
     document.getElementsByClassName("loading")[0].style.display = "block";
 
-    fetch('https://4bb29ec7c686.ngrok.io/process?' + Date.now(), {
+    fetch('https://84b3df1590ea.ngrok.io/process?' + Date.now(), {
         method: 'POST',
         body: JSON.stringify(data),
     })
