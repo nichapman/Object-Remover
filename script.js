2fb08444e8dee14c951f0280893a163c2f3bce5e
Nick Chapman <nc654@bath.ac.uk>
2021-03-16 22:26:21 +0000

    update endpoint

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ function processImage(e) {
 
     data = { 'image': image, 'mask': mask };
 
-    fetch('https://e814ed3c7565.ngrok.io/process?' + Date.now(), {
+    fetch('https://a7d5d353df0b.ngrok.io/process?' + Date.now(), {
         method: 'POST',
         body: JSON.stringify(data),
     })
