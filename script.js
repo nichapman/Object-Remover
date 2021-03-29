f6700f432cdf0dcaba6b09f54f8514ad618f0406
Nick Chapman <nc654@bath.ac.uk>
2021-03-29 14:11:55 +0100

    update host

--- script.js
+++ script.js
@@ -173,7 +173,7 @@ function processImage(e) {
 
     data = { 'image': image, 'mask': mask };
 
-    fetch('https://56031dfbbcad.ngrok.io/process?' + Date.now(), {
+    fetch('https://2b4a582f4aae.ngrok.io/process?' + Date.now(), {
         method: 'POST',
         body: JSON.stringify(data),
     })
